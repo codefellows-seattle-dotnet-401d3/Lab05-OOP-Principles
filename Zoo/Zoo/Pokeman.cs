b5351f1c2ee7cfe16f2762ec097386f114c350d5
Playingtygre <kymiehsu123@gmail.com>
2018-03-26 20:35:36 -0700

    adding classes

---
+++ Zoo/Zoo/Pokeman.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Zoo
+{
+    abstract class Pokeman
+    {
+
+        public abstract int Height { get; set; }
+        public abstract int Weight { get; set; }
+        public abstract int HitPoints { get; set;}
+        public abstract string Attack { get; set; }
+
+        public abstract bool CatchThemAll();
+
+    }
+}
