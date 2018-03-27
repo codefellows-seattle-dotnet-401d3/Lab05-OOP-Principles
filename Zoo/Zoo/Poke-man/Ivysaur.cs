2033091dbf397587e00f45d5077fb892814a9259
Playingtygre <kymiehsu123@gmail.com>
2018-03-26 21:19:42 -0700

    adding more pokemon

---
+++ Zoo/Zoo/Poke-man/Ivysaur.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Zoo.Poke_man
+{
+    class Ivysaur : Classes.PlantType
+    {
+        public override int Height { get; set; } = 180;
+        public override int Weight { get; set; } = 180;
+        public override int HitPoints { get; set; } = 180;
+        public override String Attack { get; set; } = "Solar Blast";
+
+
+        //public override abstract bool CatchThemAll(true);
+
+        public override bool CatchThemAll()
+        {
+            return true;
+        }
+    }
+}
