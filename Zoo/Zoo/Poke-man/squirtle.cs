7fb8c1c9d8a42bccb68e88f6301f12958903b053
Playingtygre <kymiehsu123@gmail.com>
2018-03-26 21:25:50 -0700

    adding more pokemon

---
+++ Zoo/Zoo/Poke-man/squirtle.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Zoo.Poke_man
+{
+    class squirtle : Classes.WaterType
+    {
+        public override int Height { get; set; } = 180;
+        public override int Weight { get; set; } = 180;
+        public override int HitPoints { get; set; } = 180;
+        public override String Attack { get; set; } = "water gun";
+
+
+        //public override abstract bool CatchThemAll(true);
+
+        public override bool CatchThemAll()
+        {
+            return true;
+        }
+    }
+}
