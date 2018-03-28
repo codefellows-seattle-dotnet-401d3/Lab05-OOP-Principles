605f586d5aab955841df0477a5142357e67ac8cb
Kevin <kevnaf@gmail.com>
2018-03-27 22:10:37 -0700

    classes done

--- Lab05/Lab05/ShockerLizzard.cs
+++ Lab05/Lab05/ShockerLizzard.cs
@@ -4,13 +4,20 @@ using System.Text;
 
 namespace Lab05
 {
-    public class DisplacerBeast : Monstrosity
+    class ShockerLizzard : Fiend
     {
         public override string Name { get; } = "DisplacerBeast";
-        public override int HP { get; set; } = 42;
+        public override int HP { get; set; } = 12;
+        public override string SubType { get; set; }
         public override string Immunity { get; set; } = "Fire, Poison";
         public override string NativePlane => base.NativePlane;
         protected override bool Hostile { get => base.Hostile; set => base.Hostile = value; }
+
+        public override string[] Abilities()
+        {
+            string[] abs = new string[0];
+            return abs;
+        }
 
         public override int ThreatLevel(int strength)
         {
